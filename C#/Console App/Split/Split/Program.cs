0fd7ee0e03fabf9a01a64237624e3fc40948c5fa
Tuan Anh <Tuan Anh@DESKTOP-O7FKRKI>
2022-04-30 10:50:57 +0700

    Upload_C#

---
+++ C#/Console App/Split/Split/Program.cs
@@ -0,0 +1,56 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System.IO;
+
+namespace Split
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pdfFilePath = @"H:\27-4-04_0001.pdf"; //đường dẫn tới file gốc
+            string outputPath = @"H:\Splited";           //Đường dẫn file xuất
+            int interval = 2;                            //Số trang muốn split
+            int pageNameSuffix = 0;
+
+            PdfReader reader = new PdfReader(pdfFilePath);
+
+            FileInfo file = new FileInfo(pdfFilePath);
+            string pdfFileName = file.Name.Substring(0, file.Name.LastIndexOf(".")) + "-";
+
+            Program obj = new Program();
+
+            for (int pageNumber = 1; pageNumber <= reader.NumberOfPages; pageNumber += interval)
+            {
+                pageNameSuffix++;
+                string newPdfFileName = string.Format(pdfFileName + "{0}", pageNameSuffix);
+                obj.SplitAndSaveInterval(pdfFilePath, outputPath, pageNumber, interval, newPdfFileName);
+            }
+        }
+
+        private void SplitAndSaveInterval(string pdfFilePath, string outputPath, int startPage, int interval, string pdfFileName)
+        {
+            using (PdfReader reader = new PdfReader(pdfFilePath))
+            {
+                Document document = new Document();
+                PdfCopy copy = new PdfCopy(document, new FileStream(outputPath + "\\" + pdfFileName + ".pdf", FileMode.Create));
+                document.Open();
+
+                for (int pagenumber = startPage; pagenumber < (startPage + interval); pagenumber++)
+                {
+                    if (reader.NumberOfPages >= pagenumber)
+                    {
+                        copy.AddPage(copy.GetImportedPage(reader, pagenumber));
+                    }
+                    else
+                    {
+                        break;
+                    }
+
+                }
+
+                document.Close();
+            }
+        }
+    }
+}
